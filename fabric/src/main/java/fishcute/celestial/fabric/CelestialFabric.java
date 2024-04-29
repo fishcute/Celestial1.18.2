0a285f6d920995e44219d2db9238ada868a86b6c
block36_ <block36not33@gmail.com>
2024-04-29 18:04:32 -0400

    Init (does not work)

---
+++ fabric/src/main/java/fishcute/celestial/fabric/CelestialFabric.java
@@ -0,0 +1,11 @@
+package fishcute.celestial.fabric;
+
+import fishcute.celestial.Celestial;
+import net.fabricmc.api.ModInitializer;
+
+public class CelestialFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        Celestial.init();
+    }
+}
